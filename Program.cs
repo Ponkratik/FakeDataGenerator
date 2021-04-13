706f07ecfac87a4d2970bc50b3a047277c1bbaf3
Ponkratik <63253443+Ponkratik@users.noreply.github.com>
2021-04-13 23:37:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+
+namespace Task4
+{
+    public class Person
+    {
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Adress { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = Convert.ToInt32(args[0]);
+
+            Faker<Person> generatorPerson = GetPersonGenerator(args[1]);
+
+            List<Person> persons = generatorPerson.Generate(count);
+
+            PrintPersons(persons);
+        }
+
+        private static Faker<Person> GetPersonGenerator(string reg)
+        {
+            return new Faker<Person>(reg)
+            .RuleFor(x => x.FullName, f => f.Name.FullName())
+            .RuleFor(x => x.Phone, f => f.Phone.PhoneNumberFormat())
+            .RuleFor(x => x.Adress, f => f.Address.FullAddress());
+        }
+        private static void PrintPersons(List<Person> persons)
+        {
+            for (int i = 0; i < persons.Count; i++)
+            {
+                Console.WriteLine($"{persons[i].FullName};+{persons[i].Phone};{persons[i].Adress}");
+            }
+        }
+    }
+}
